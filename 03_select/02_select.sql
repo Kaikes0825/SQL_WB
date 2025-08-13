33555a79c2edd418bcc00eb0c3febfd7e707c207
Kaikes0825 <knittelkai0@gmail.com>
2025-08-13 12:04:51 +0200

    unscharfe suche

--- 03_select/02_select.sql
+++ 03_select/02_select.sql
@@ -21,3 +21,14 @@ SELECT
 FROM languages.contacts
 WHERE city = 'Berlin' 
 ;       
+
+
+--unscharfe suche
+
+
+SELECT
+        id,
+        city
+FROM languages.contacts
+WHERE city = LIKE '%furt'
+;
